fde2643f48791aa224ad1af8151359d538f60dac
oussama <toumi11oussama@gmail.com>
2023-06-22 13:29:16 -0700

    88

--- pall.c
+++ pall.c
@@ -5,18 +5,18 @@
  * @stack1: stack head
  * @counter: not used
  * Return: no return
-*/
+ */
 void f_pall(stack_t **stack1, unsigned int counter)
 {
-        stack_t *h;
-        (void)counter;
+	stack_t *h;
+	(void)counter;
 
-        h = *stack1;
-        if (h == NULL)
-                return;
-        while (h)
-        {
-                printf("%d\n", h->n);
-                h = h->next;
-        }
+	h = *stack1;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 }
--- push.c
+++ push.c
@@ -5,39 +5,41 @@
  * @stack1: stack head
  * @som: line_number
  * Return: no return
-*/
+ */
 void f_push(stack_t **stack1, unsigned int som)
 {
-        int n, a = 0;
-        char *arg;
-        arg = strtok(NULL, " \n\t");
-        if (arg != NULL)
-        {
-                if (arg[0] == '-')
-                        a++;
-                for (; arg[a] != '\0'; a++)
-                {
-                        if (arg[a] > 57 || arg[a] < 48)
-                                break;
-                }
-                if (arg[a] != '\0')
-                {
-                        fprintf(stderr, "L%d: usage: push integer\n", som);
-                        fclose(bus.file);
-                        free(bus.content);
-                        free_stack(*stack1);
-                        exit(EXIT_FAILURE);
-                }
-        }
-        else
-        {
-                fprintf(stderr, "L%d: usage: push integer\n", som);
-                fclose(bus.file);
-                free(bus.content);
-                free_stack(*stack1);
-                exit(EXIT_FAILURE);
-        }
-        n = atoi(arg);
-        if (bus.lifi == 0)
-                addnode(stack1, n);
+	int n, a = 0;
+	char *arg;
+
+	arg = strtok(NULL, " \n\t");
+	if (arg != NULL)
+	{
+		if (arg[0] == '-')
+			a++;
+		for (; arg[a] != '\0'; a++)
+		{
+			if (arg[a] > 57 || arg[a] < 48)
+				break;
+		}
+		if (arg[a] != '\0')
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", som);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*stack1);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", som);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack1);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(arg);
+	if (bus.lifi == 0)
+		addnode(stack1, n);
 }
+
